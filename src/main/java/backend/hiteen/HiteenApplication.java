96b85dcfd1d83350a7023190e1e28e9469d2fa10
fallkim <fs728kimgc@naver.com>
2025-03-17 21:54:01 +0900

    Initial commit

---
+++ src/main/java/backend/hiteen/HiteenApplication.java
@@ -0,0 +1,13 @@
+package backend.hiteen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HiteenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HiteenApplication.class, args);
+	}
+
+}
