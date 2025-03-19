c8ca84762950b260b7fc12a6f931e199facbc938
suyeonnii <suyeon9288@gmail.com>
2025-03-19 22:10:11 +0900

    feat:member 엔티티 생성

--- src/main/java/backend/hiteen/member/entity/Member.java
+++ src/main/java/backend/hiteen/member/entity/Member.java
@@ -1,4 +1,17 @@
 package backend.hiteen.member.entity;
 
+import backend.hiteen.board.entity.Board;
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
 public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="member_id")
+    private Long id;
+    @OneToMany(mappedBy = "member")
+    private List<Board> boards;
 }
