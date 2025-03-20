5e29d849cd014d1ddbe8c1fbd614db0fac845002
suyeonnii <suyeon9288@gmail.com>
2025-03-20 13:13:48 +0900

    fix:member 예외처리 삭제

--- src/main/java/backend/hiteen/board/service/BoardService.java
+++ src/main/java/backend/hiteen/board/service/BoardService.java
@@ -6,11 +6,13 @@ import backend.hiteen.board.entity.Board;
 import backend.hiteen.board.repository.BoardRepository;
 import backend.hiteen.member.entity.Member;
 import backend.hiteen.member.repository.MemberRepository;
-import jakarta.transaction.Transactional;
+import jakarta.validation.constraints.Null;
+import org.springframework.transaction.annotation.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -23,11 +25,31 @@ public class BoardService {
     @Transactional
     public BoardResponse createBoard(final BoardCreateRequest request){
         Member member=memberRepository.findById(request.getMemberId())
-                .orElseThrow(()->new IllegalArgumentException("존재하지 않는 회원입니다."));
+                .orElse(null);
+//        Throw(()->new IllegalArgumentException("존재하지 않는 회원입니다."));
 
         Board board=Board.create(member,request.getTitle(),request.getContent());
         boardRepository.save(board);
         return new BoardResponse(board);
     }
+
+
+//    @Transactional(readOnly = true)
+//    //게시글 전체 조회 - 모든 사용자에 대한
+//    public List<BoardResponse> getAllBoards(){
+//        List<Board> boards =boardRepository.findAll();
+//        return boards.stream().map(BoardResponse::new)
+//                .collect(Collectors.toList());
+//
+//    }
+
+
+//    //게시글 단일 조회 - 모든 사용자에 대한
+//    @Transactional(readOnly=true)
+//    public BoardResponse getBoardById(Long boardId){
+//        Board board=boardRepository.findById(boardId)
+//                .orElseThrow(()->new IllegalArgumentException("게시글이 존재하지 않습니다."));
+//        return new BoardResponse(board);
+//    }
 
 }
