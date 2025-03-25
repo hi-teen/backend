15f22ab1d1f28c1c2a8c68fa358db5bc1afd9753
suyeonnii <suyeon9288@gmail.com>
2025-03-25 21:11:08 +0900

    feat:swagger 문서에 API 추가

--- src/main/java/backend/hiteen/love/controller/LoveController.java
+++ src/main/java/backend/hiteen/love/controller/LoveController.java
@@ -1,6 +1,7 @@
 package backend.hiteen.love.controller;
 
 import backend.hiteen.love.service.LoveService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -12,12 +13,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping
-@Tag(name = "좋아요", description = "Love")
+@Tag(name = "Love", description = "좋아요 API")
 public class LoveController {
 
     private final LoveService loveService;
 
     @PostMapping("/member/{memberId}/board/{boardId}/love")
+    @Operation(summary = "좋아요", description = "사용자가 좋아요를 추가하거나 취소합니다.")
     public ResponseEntity<String> loveBoard(@PathVariable Long memberId, @PathVariable Long boardId){
         String message=loveService.updateLoveBoard(memberId,boardId);
         return ResponseEntity.ok(message);
