8e16e05f4282f1d51b0191f4a6154c7e97716eb2
fallkim <fs728kimgc@naver.com>
2025-03-26 23:53:42 +0900

    feat :게시글에 대한 댓글 전체 조회 구현

--- src/main/java/backend/hiteen/comment/repository/CommentRepository.java
+++ src/main/java/backend/hiteen/comment/repository/CommentRepository.java
@@ -6,9 +6,14 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
     @Query("SELECT MAX(c.anonymousNumber) FROM Comment c WHERE c.board.id = :boardId")
     Integer findMaxAnonymousNumberByBoardId(@Param("boardId") Long boardId);
+
+    @Query("SELECT c FROM Comment c WHERE c.board.id = :boardId AND c.parentComment IS NULL ORDER BY c.createdAt ASC")
+    List<Comment> findRootsByBoardId(@Param("boardId") Long boardId);
 }
