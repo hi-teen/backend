a7c68baf0beb90740d8e6543eec960bf4ae5e862
fallkim <fs728kimgc@naver.com>
2025-06-16 11:50:37 +0900

    test: test controller 작성

---
+++ src/main/java/backend/hiteen/global/config/TestController.java
@@ -0,0 +1,13 @@
+package backend.hiteen.global.config;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/")
+    public String test() {
+        return "안녕하슈~";
+    }
+}
