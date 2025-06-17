08b538c195da76ad705680ad0688d3b534218355
fallkim <fs728kimgc@naver.com>
2025-06-17 19:54:41 +0900

    fix: 스웨거 특정 경로 허용

--- src/main/java/backend/hiteen/global/config/SecurityConfig.java
+++ src/main/java/backend/hiteen/global/config/SecurityConfig.java
@@ -33,8 +33,8 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
                                 "/",
-                                "/auth/**",
-                                "/members/sign-up",
+                                "/api/v1/auth/**",
+                                "/api/v1/members/sign-up",
                                 "/swagger-ui/**",
                                 "/v3/api-docs/**"
                         ).permitAll()
