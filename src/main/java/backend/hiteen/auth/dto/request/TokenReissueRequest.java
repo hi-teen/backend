409e1aab01ae4e7809de0c57e459a625e79da55d
suyeonnii <suyeon9288@gmail.com>
2025-06-29 17:12:03 +0900

    feat: 토큰 재발급 기능 구현

--- src/main/java/backend/hiteen/auth/dto/request/TokenReissueRequest.java
+++ src/main/java/backend/hiteen/auth/dto/request/TokenReissueRequest.java
@@ -1,2 +1,8 @@
-package backend.hiteen.auth.dto.request;public class TokenReissueRequest {
+package backend.hiteen.auth.dto.request;
+
+import lombok.Getter;
+
+@Getter
+public class TokenReissueRequest {
+    private String refreshToken;
 }
