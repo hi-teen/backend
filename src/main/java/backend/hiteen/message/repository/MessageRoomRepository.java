e4f415185d9138ad29dba1920deddd4b0ab507d6
fallkim <fs728kimgc@naver.com>
2025-07-26 17:12:15 +0900

    feat : 내가 참여한 쪽지방 리스트 구현 및 읽은 갯수처리

--- src/main/java/backend/hiteen/message/repository/MessageRoomRepository.java
+++ src/main/java/backend/hiteen/message/repository/MessageRoomRepository.java
@@ -6,16 +6,27 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface MessageRoomRepository extends JpaRepository<MessageRoom, Long> {
 
-    @Query("select mr from MessageRoom mr " +
-            "where mr.board.id = :boardId " +
-            "and ((mr.senderId = :id1 and mr.receiverId = :id2) " +
-            "or (mr.senderId = :id2 and mr.receiverId = :id1))")
+    @Query("""
+            select mr from MessageRoom mr
+            where mr.board.id = :boardId
+            and ((mr.senderId = :id1 and mr.receiverId = :id2)
+            or (mr.senderId = :id2 and mr.receiverId = :id1))
+            """)
     Optional<MessageRoom> findByBoardIdAndParticipants(
             @Param("boardId") Long boardId,
             @Param("id1") Long id1,
             @Param("id2") Long id2);
+
+    @Query("""
+                    select mr
+                    from MessageRoom mr
+                    where mr.senderId = :memberId or mr.receiverId = :memberId
+                    order by (select max(m.createdAt) from Message m where m.messageRoom = mr) desc
+            """)
+    List<MessageRoom> findAllByMember(@Param("memberId") Long memberId);
 }
