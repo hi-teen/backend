3a98ed1d2e7a76d6691a5334933c28d89fc12a8d
fallkim <fs728kimgc@naver.com>
2025-07-31 18:06:26 +0900

    fix : security path 허용

--- src/main/java/backend/hiteen/auth/jwt/JwtAuthenticationFilter.java
+++ src/main/java/backend/hiteen/auth/jwt/JwtAuthenticationFilter.java
@@ -7,7 +7,6 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
@@ -16,7 +15,6 @@ import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
-import java.util.List;
 
 @Component
 @RequiredArgsConstructor
@@ -60,8 +58,11 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         return path.equals("/") ||
                 path.startsWith("/swagger-ui") ||
                 path.startsWith("/v3/api-docs") ||
-                path.startsWith("/auth") ||
-                path.equals("/members/sign-up");
+                path.startsWith("/api/v1/auth") ||
+                path.equals("/api/v1/members/sign-up") ||
+                path.startsWith("/api/v1/school-meal") ||
+                path.startsWith("/api/v1/timetable") ||
+                path.startsWith("/schools");
     }
 
 
